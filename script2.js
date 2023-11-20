7689c9bba705b0deeba9ba58c6299fe96916d101
Tarun Kumar <tarunkumar2407@gmail.com>
2023-11-20 23:49:43 +0530

    initial commit

--- script2.js
+++ script2.js
@@ -1,10 +1,29 @@
 // console.log("hello world");
 
 // console.log(a)
-console.log(b)
-let a = 10;
-var b = 15;
-console.log(a);
-console.log(b);
-console.log(window.a)
-console.log(window.b)
+// console.log(b)
+// let a = 10;
+// var b = 15;
+// console.log(a);
+// console.log(b);
+// console.log(window.a)
+// console.log(window.b)
+// console.log(window.c)
+// console.log(window.d)
+
+// var a = 200;
+// {
+//     let a = 20;
+//     console.log(a)
+// }
+// console.log(a)
+
+
+let num = 10;
+console.log(Number.parseFloat(num))
+
+let a = "My name is Tarun"
+// let str = Number.parseInt(a)
+let str = "" + parseFloat(a)
+console.log(typeof str)
+console.log(typeof a)
