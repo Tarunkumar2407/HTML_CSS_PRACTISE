9f44a1deabe04f1ae45141b2f4c884de3b313d41
Tarun Kumar <tarunkumar2407@gmail.com>
2023-11-21 11:24:25 +0530

    initial commit

---
+++ react-practise/src/components/Class.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+
+class User extends Component{
+    render() {
+        return(
+            <h1>Hello i am class component</h1>
+        )
+    }
+}
+export default User
---
+++ react-practise/src/components/Functional.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Functional = () => {
+    return (
+        <>
+            <h1>hello world</h1>
+        </>
+    )
+}
+export default Functional
